c0936859eb609958caf68691bf32ed7ed7738d31
murtada.abdalgalil <murtada199815@gmail.com>
2023-03-12 15:17:00 +0200

    Basic Auth

--- src/main/java/com/testlog/demo/security/SecurityConfig.java
+++ src/main/java/com/testlog/demo/security/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.stereotype.Service;
 
 
 @Configuration
